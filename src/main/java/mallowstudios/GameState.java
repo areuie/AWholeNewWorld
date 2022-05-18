9cef758e480aef44bea18bc804e6778fb371903c
areuie <63759859+areuie@users.noreply.github.com>
2022-05-17 22:11:15 -0400

    Delete src/main directory

--- src/main/java/mallowstudios/GameState.java
+++
@@ -1,7 +0,0 @@
-package mallowstudios;
-
-public enum GameState {
-        Menu(),
-        Game(),
-        Exit();
-}
--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module awholenewworld.awholenewworld {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires transitive java.desktop;
-    requires javafx.graphics;
-
-    opens mallowstudios to javafx.fxml;
-    exports mallowstudios;
-}
