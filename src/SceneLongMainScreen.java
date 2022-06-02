16f1e9f014f46a4a2b46c7565561f3058de8b98e
areuie <63759859+areuie@users.noreply.github.com>
2022-06-02 13:08:30 -0400

    new long screen

--- src/SceneLongMainScreen.java
+++ src/SceneLongMainScreen.java
@@ -24,23 +24,21 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.EnumMap;
-import java.util.Map;
 
 public class SceneLongMainScreen extends JPanel{
     /** This variable stores the background */
-    private BufferedImage bg, spriteStanding,spriteAni1,spriteAni2,spriteAni3,spriteAni4;
+    private BufferedImage bg, spriteStanding,spriteAni1,spriteAni2,spriteAni3,spriteAni4, bSchool, bWork, bPost, bImm;
+    private Building[] buildingList = new Building[4];
     /** This variable stores the x coord of the background */
     int backgroundX = 0, beforeBackgroundX = 0;
     /** This variable checks if the player is walking or not */
     boolean isWalking = false;
     int typeImage = 0;//0 is still, 1,2,3,4 are walking/motion
     Timer timer1 = new Timer(100, new ActionListener() {
+        @Override
         public void actionPerformed(ActionEvent e) {
             if(isWalking) {
                 if (typeImage >4) typeImage = 1;
@@ -52,17 +50,13 @@ public class SceneLongMainScreen extends JPanel{
             if(beforeBackgroundX==backgroundX){
                 isWalking = false;
                 typeImage = 0;
+                System.out.println("!isWalking");
             }
         }
     });
 
     //Keep track of the time when user stays in place, when x is not changing.
-    Timer timer2 = new Timer(150, new ActionListener() {
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            beforeBackgroundX = backgroundX;
-        }
-    });
+    Timer timer2 = new Timer(150, e -> beforeBackgroundX = backgroundX);
 
     /**
      * The constructor of the panel
@@ -75,10 +69,15 @@ public class SceneLongMainScreen extends JPanel{
             spriteAni2 = ImageIO.read(new File("src/img/pixil-frame-Female2.png"));
             spriteAni3 = ImageIO.read(new File("src/img/pixil-frame-Female3.png"));
             spriteAni4 = ImageIO.read(new File("src/img/pixil-frame-Female4.png"));
+
+//            bSchool = ImageIO.read(new File("src/img/pixil-frame-Female3.png"));
+//            bWork = ImageIO.read(new File("src/img/pixil-frame-Female3.png"));
+//            bPost = ImageIO.read(new File("src/img/pixil-frame-Female3.png"));
+//            bImm = ImageIO.read(new File("src/img/pixil-frame-Female3.png"));
         } catch (IOException e) {
             e.printStackTrace();
         }
-        if (Game.gameState == 9) {
+        if (Game.gameState == 12) {
             timer1.start();
             timer2.start();
         }
@@ -87,6 +86,20 @@ public class SceneLongMainScreen extends JPanel{
         ActionMap am = getActionMap();
 
         im.put(KeyStroke.getKeyStroke("LEFT"), "left");
+        im.put(KeyStroke.getKeyStroke("RIGHT"), "right");
+
+        am.put("left", new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("left " + backgroundX);
+                if (backgroundX + 1 <= 0) {
+                    backgroundX+=5;
+                    isWalking = true;
+                    System.out.println("Iswalking");
+                }
+                repaint();
+            }
+        });
 
         am.put("right", new AbstractAction() {
             @Override
@@ -122,14 +135,18 @@ public class SceneLongMainScreen extends JPanel{
             Image background = bg.getScaledInstance(2400, 600, Image.SCALE_DEFAULT);
             g.drawImage(background, backgroundX, 0, this);
         }
-        if(true){
-            Image sprite = spriteStanding.getScaledInstance(spriteStanding.getWidth()*6, spriteStanding.getHeight()*6, Image.SCALE_DEFAULT); ;
-            if (typeImage == 0) sprite = spriteStanding.getScaledInstance(spriteStanding.getWidth()*6, spriteStanding.getHeight()*6, Image.SCALE_DEFAULT);
-            else if (typeImage == 1) sprite = spriteAni1.getScaledInstance(spriteAni1.getWidth()*6, spriteAni1.getHeight()*6, Image.SCALE_DEFAULT);
-            else if (typeImage == 2) sprite = spriteAni2.getScaledInstance(spriteAni2.getWidth()*6, spriteAni2.getHeight()*6, Image.SCALE_DEFAULT);
-            else if (typeImage == 3) sprite = spriteAni3.getScaledInstance(spriteAni3.getWidth()*6, spriteAni3.getHeight()*6, Image.SCALE_DEFAULT);
-            else if (typeImage == 4 || typeImage ==5) sprite = spriteAni4.getScaledInstance(spriteAni4.getWidth()*6, spriteAni4.getHeight()*6, Image.SCALE_DEFAULT);
-            g.drawImage(sprite,10,360, this);
-        }
+
+        Image sprite = spriteStanding.getScaledInstance(spriteStanding.getWidth()*6, spriteStanding.getHeight()*6, Image.SCALE_DEFAULT); ;
+        if (typeImage == 0) sprite = spriteStanding.getScaledInstance(spriteStanding.getWidth()*6, spriteStanding.getHeight()*6, Image.SCALE_DEFAULT);
+        else if (typeImage == 1) sprite = spriteAni1.getScaledInstance(spriteAni1.getWidth()*6, spriteAni1.getHeight()*6, Image.SCALE_DEFAULT);
+        else if (typeImage == 2) sprite = spriteAni2.getScaledInstance(spriteAni2.getWidth()*6, spriteAni2.getHeight()*6, Image.SCALE_DEFAULT);
+        else if (typeImage == 3) sprite = spriteAni3.getScaledInstance(spriteAni3.getWidth()*6, spriteAni3.getHeight()*6, Image.SCALE_DEFAULT);
+        else if (typeImage == 4 || typeImage ==5) sprite = spriteAni4.getScaledInstance(spriteAni4.getWidth()*6, spriteAni4.getHeight()*6, Image.SCALE_DEFAULT);
+
+//        for (int i = 0; i < buildingList.length; i++) {
+//            g.drawImage(buildingList[i].getType(), buildingList[i].getX(), buildingList[i].getY(), this);
+//        }
+
+        g.drawImage(sprite,10,360, this);
     }
 }
