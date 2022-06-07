93bef1febbe84c4fc2b082db7455e80cc9283057
mona1afshar <mona1afshar@gmail.com>
2022-06-07 14:50:52 -0400

    updated ending

--- src/Game.java
+++ src/Game.java
@@ -52,7 +52,7 @@ public class Game {
     /** This variable stores the current screen*/
     public static JPanel screen;
     /** This variable stores the game state */
-    public static int gameState = 12;
+    public static int gameState = 11;
     /** This variable stores the amount of money the player has */
     public static int money = 500;
     /** This variable is the font of the game*/
@@ -243,6 +243,17 @@ public class Game {
                 frame.setVisible(true);
 
                 screen = longScreen;
+                gameState = 0;
+            } else if (gameState == 14) {
+                frame.remove(screen);
+
+                SceneEnding end = new SceneEnding();
+                frame.add(end);
+                frame.pack();
+                frame.setLocationRelativeTo(null);
+                frame.setVisible(true);
+
+                screen = end;
                 gameState = 0;
             }
 
